58464fe81745114e98b158cf3ac82e62731bacb8
GermanJaf <jafoestrada05@gmail.com>
2026-06-19 13:36:41 -0600

    Prueba
    Esto solo se hizo para poder entender como funcionaba GIT

---
+++ Pokemon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonJuegoProyecto
+{
+    internal class Pokemon
+    {
+        public class Pokemon
+        {
+            public string Nombre { get; set; }
+            public string Tipo { get; set; }
+            public int Nivel { get; set; }
+            public Pokemon(string nombre, string tipo, int nivel)
+            {
+                Nombre = nombre;
+                Tipo = tipo;
+                Nivel = nivel;
+            }
+            public void MostrarInformacion()
+            {
+                Console.WriteLine($"Nombre: {Nombre}");
+                Console.WriteLine($"Tipo: {Tipo}");
+                Console.WriteLine($"Nivel: {Nivel}");
+            }
+        } 
+    }
+}
