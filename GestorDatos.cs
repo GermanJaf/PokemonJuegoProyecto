fe22dde8ae1e62a61fd5894e516066bfa6a84a31
GermanJaf <jafoestrada05@gmail.com>
2026-06-21 20:11:20 -0600

    Conexion a SQLite
    Ahora voy a meter el sql al programa

---
+++ GestorDatos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using SQLiteUtil;
+
+namespace PokemonJuegoProyecto
+{
+    public class GestorDatos
+    {
+        private SqliteConnection conn;
+
+        public GestorDatos()
+        {
+            conn = new SqliteConnection("Data Source=PokemonJuego.db");
+            conn.Open();
+        }
+    }
+}
