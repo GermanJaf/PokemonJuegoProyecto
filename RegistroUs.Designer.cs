6ad60189250baad599fc1968ede71b121db60a67
Reginacgl <a20208001@alumnos.uady.mx>
2026-06-30 02:15:39 -0600

    Update RegistroUs.Designer.cs

--- RegistroUs.Designer.cs
+++ RegistroUs.Designer.cs
@@ -120,7 +120,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(476, 450);
             this.Controls.Add(this.btnRegistrar);
             this.Controls.Add(this.txtConfirmarContrasena);
             this.Controls.Add(this.txtContrasena);
