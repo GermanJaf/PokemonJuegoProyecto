423170e6733dde59beee4e19a20f967d16266846
RodrigoFC-28 <rodrigojesusflorescih@gmail.com>
2026-07-08 19:03:07 -0600

    Update PantallaRendimientoPK.Designer.cs

--- PantallaRendimientoPK.Designer.cs
+++ PantallaRendimientoPK.Designer.cs
@@ -43,10 +43,9 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Rockwell", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(119, 51);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(178, 78);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(509, 48);
+            this.label1.Size = new System.Drawing.Size(775, 76);
             this.label1.TabIndex = 0;
             this.label1.Text = "Analiza el desempeño de tus Pokémon en combate \r\ny verifica cuántas victorias ha " +
     "logrado cada uno";
@@ -55,12 +54,11 @@
             // ListaGanadasPokemon
             // 
             this.ListaGanadasPokemon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.ListaGanadasPokemon.Location = new System.Drawing.Point(11, 136);
-            this.ListaGanadasPokemon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ListaGanadasPokemon.Location = new System.Drawing.Point(16, 209);
             this.ListaGanadasPokemon.Name = "ListaGanadasPokemon";
             this.ListaGanadasPokemon.RowHeadersWidth = 51;
             this.ListaGanadasPokemon.RowTemplate.Height = 24;
-            this.ListaGanadasPokemon.Size = new System.Drawing.Size(659, 291);
+            this.ListaGanadasPokemon.Size = new System.Drawing.Size(988, 448);
             this.ListaGanadasPokemon.TabIndex = 1;
             this.ListaGanadasPokemon.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ListaGanadasPokemon_CellContentClick);
             // 
@@ -69,10 +67,9 @@
             this.buttonVolverPT.BackColor = System.Drawing.Color.Transparent;
             this.buttonVolverPT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonVolverPT.Font = new System.Drawing.Font("SimSun", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonVolverPT.Location = new System.Drawing.Point(251, 460);
-            this.buttonVolverPT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonVolverPT.Location = new System.Drawing.Point(376, 708);
             this.buttonVolverPT.Name = "buttonVolverPT";
-            this.buttonVolverPT.Size = new System.Drawing.Size(169, 39);
+            this.buttonVolverPT.Size = new System.Drawing.Size(254, 60);
             this.buttonVolverPT.TabIndex = 2;
             this.buttonVolverPT.Text = "Volver";
             this.buttonVolverPT.UseVisualStyleBackColor = false;
@@ -83,10 +80,9 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Stencil", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(212, 13);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(318, 20);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(297, 29);
+            this.label2.Size = new System.Drawing.Size(440, 43);
             this.label2.TabIndex = 3;
             this.label2.Text = "Panel de RENDIMIENTO:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -95,9 +91,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::PokemonJuegoProyecto.Properties.Resources.hitmonchan;
-            this.pictureBox1.Location = new System.Drawing.Point(24, 31);
+            this.pictureBox1.Location = new System.Drawing.Point(36, 48);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(76, 68);
+            this.pictureBox1.Size = new System.Drawing.Size(114, 105);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -105,10 +102,10 @@
             // 
             // PantallaRendimientoPK
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::PokemonJuegoProyecto.Properties.Resources.FONDORENDIMIENTO1;
-            this.ClientSize = new System.Drawing.Size(684, 531);
+            this.ClientSize = new System.Drawing.Size(1026, 817);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonVolverPT);
@@ -116,10 +113,10 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "PantallaRendimientoPK";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RendimientoPK";
             this.Load += new System.EventHandler(this.PantallaRendimientoPK_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ListaGanadasPokemon)).EndInit();
